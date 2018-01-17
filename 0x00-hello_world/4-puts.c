a6c3b35cbc18800046663b4f5bbc9bad9d6723ce
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-01-17 22:30:48 +0000

    learn betty, c program that prints out 'Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a message using the puts function
+ * Return: 0
+ */
+int main(void)
+{
+	char message[100] = "\"Programming is like building a multilingual puzzle";
+
+	puts(message);
+	return (0);
+}
