16944ceebfbf27705b4f0638b194a8f5831bb2a8
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-01-18 22:40:17 +0000

    change for loop logic for C program that prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; ++i)
+	for (i = 48; i <= 56; ++i)
 	{
 		putchar(i);
 		putchar(',');
 		putchar(' ');
 	}
-
+	putchar(i);
 	putchar('\n');
 
 	return (0);
