7475d83739e3b1edd4c3967fe34055d04bef34bb
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-01-19 21:43:31 +0000

    fixed type, styled file to betty style guide

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main - prints a phrase to the standar error output 
+ * main - prints a phrase to the standar error output
  *
  *
  * Return: 0;
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
