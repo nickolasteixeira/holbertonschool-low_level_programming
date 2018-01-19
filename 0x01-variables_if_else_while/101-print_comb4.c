fbb3d5178d57fa1437fbecf4ac8f69c6eb953cee
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-01-19 02:14:56 +0000

    add C program that prints combinations of 3 digits, non recurring

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of three digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i <= '9'; ++i)
+	{
+		for (j = '0'; j <= '9'; ++j)
+		{
+			if (j > i)
+			{
+				for (k = '0'; k <= '9'; ++k)
+				{
+					if (k > j)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(k);
+						if (i == '7' && j == '8' && k == '9')
+						{
+							break;
+						}
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
