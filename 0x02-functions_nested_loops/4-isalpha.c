52af284b5f0cb5c7eeb02fac6c5adafbb3d8700e
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-01-23 19:05:58 +0000

    add function that cheks for alphabetic characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+/**
+ * 4-isalpha.c - checks for alphabetic character
+ *
+ * Description: Returns 1 if c is a letter, lowercase or uppercase
+ * Return: 1
+ */
+
+int _isalpha(int c)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; ++i)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+
+	for (i = 'A'; i <= 'Z'; ++i)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
