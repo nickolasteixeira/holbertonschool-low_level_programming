0b0f14356417ca07b9cdda5eb27a8759d09fe4c7
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-01-23 01:52:59 +0000

    header files, putchar function, function that prints "Holberton" by using header files

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - prints "Holberton" using the holberton.h standard library
+ *
+ * Return: 0;
+ */
+
+int main(void)
+{	
+	char string[10] = "Holberton";
+	int i;
+
+	for (i = 0; i < 10; ++i)
+		_putchar(string[i]);
+	
+	_putchar('\n');
+
+	return (0);	
+}
