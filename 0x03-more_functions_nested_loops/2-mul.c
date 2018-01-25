b75e47a36d2d34cf4fc36bacae5b0918fe2435ed
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-01-25 18:54:52 +0000

    function that multiplies two integers

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - returns the product of a * b
+ * @a: one integer to multipy
+ * @b: another integer to mulitply
+ *
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
