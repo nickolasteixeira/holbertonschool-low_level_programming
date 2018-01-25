dd4020dd4e73b4cd9569b706bdfd3805cbd04d2e
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-01-25 22:43:08 +0000

    betty style changes to 4 and 5

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -1,3 +1,5 @@
+#include "holberton.h"
+
 /**
  * more_numbers - prints 10 times the numbers from 0 to 14
  *
@@ -10,10 +12,10 @@ void more_numbers(void)
 
 	int j;
 
-	while(i <= 10)
+	while (i <= 10)
 	{
 		j = 0;
-		
+
 		while (j <= 14)
 		{
 			if (j < 10)
