64e55ab604f68169d0abfb3163ccd7d1615872aa
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-01-25 23:02:26 +0000

    remove a _putchar, change logic

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -18,13 +18,13 @@ void more_numbers(void)
 
 		while (j <= 14)
 		{
-			if (j < 10)
-				_putchar(j + '0');
-			else if (j > 10)
+
+			if (j > 10)
 			{
 				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
+
 			}
+			_putchar((j % 10) + '0');
 			++j;
 		}
 		++i;
