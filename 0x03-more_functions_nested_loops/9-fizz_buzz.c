d918645cfd1f03a05fef10f38a4520e31d99cd05
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-01-26 00:41:56 +0000

    chnage logic to not have space after last print

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,27 +1,57 @@
 #include <stdio.h>
 
 /**
+ * print_fizz_space - prints message with space
+ * @i: integer to compare
+ *
+ * Return: void
+ */
+void print_fizz_space(int i)
+{
+	if (i % 3 == 0 && i % 5 == 0)
+		printf("FizzBuzz ");
+	else if (i % 3 == 0)
+		printf("Fizz ");
+	else if (i % 5 == 0)
+		printf("Buzz ");
+	else
+		printf("%d ", i);
+}
+
+/**
+ * print_fizz_nospace - prints message with no space
+ * @i: integer to compare
+ *
+ * Return: void
+ */
+void print_fizz_nospace(int i)
+{
+	if (i % 3 == 0 && i % 5 == 0)
+		printf("FizzBuzz");
+	else if (i % 3 == 0)
+		printf("Fizz");
+	else if (i % 5 == 0)
+		printf("Buzz");
+	else
+		printf("%d", i);
+}
+
+/**
  * main - prints fizz, buzz, or fizzbuzz
  *
  *
  * Return: 0;
  */
-
 int main(void)
 {
 	int i = 1;
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
-		else if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
+		if (i != 100)
+			print_fizz_space(i);
 		else
-			printf("%d ", i);
-
+			print_fizz_nospace(i);
 		++i;
 	}
 
