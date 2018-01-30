d6b3a4276f5d2db021e4bd1c8d9771b36c8c07e9
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-01-30 16:30:01 +0000

    change file name

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of the string in the parameter
+ * @s: the string
+ *
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	int i = 0;
+	while (*s != '\0')
+	{
+		counter++;
+		*s++;
+		i++;
+	}
+
+	return (counter);
+}
