d2588279c19cf6c84e5f1daa41726a25bdfb9bfe
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-01 23:51:50 +0000

    remove unused variable, change function name in comments

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- * *leet - encodes a string to 1337
+ * leet - encodes a string to 1337
  * @s: the string to encode
  *
  * Return: pointer to the string
  */
 char *leet(char *s)
 {
-	unsigned int i, x, j;
+	unsigned int i, j;
 	char cases[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	char replacement[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
