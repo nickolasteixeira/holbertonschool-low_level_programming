43b081ca004d32535c4afe748789faf4850ec945
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-02 01:48:11 +0000

    add white space after each character in arrays

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -9,9 +9,21 @@ char *rot13(char *s)
 {
 	unsigned int i = 0;
 	unsigned int j;
-	char characters[52] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	char characters[52] = {
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+		'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+		'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+		'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z'};
 
-	char rotate13[52] = {'n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M'};
+	char rotate13[52] = {
+		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+		'h', 'i', 'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q',
+		'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A',
+		'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M'};
 
 	while (s[i] != '\0')
 	{
@@ -20,12 +32,12 @@ char *rot13(char *s)
 		{
 			if (s[i] == characters[j])
 			{
-				s[i] = rotate13[j];			
+				s[i] = rotate13[j];
 				break;
 			}
 			++j;
 		}
-		++i;	
+		++i;
 	}
 	return (s);
 }
