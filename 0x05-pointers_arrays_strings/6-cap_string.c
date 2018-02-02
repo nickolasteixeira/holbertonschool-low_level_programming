feb067bf51ba7e291fcea40e11d1804df8f707df
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-02 03:24:44 +0000

    logic change to using switch statement

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,5 @@
 #include "holberton.h"
 /**
- * check_condition - checks if a character is a special character
- * @t: the character to check
- *
- * Return: a character
- */
-char check_condition(char t)
-{
-	int array[14] = {
-		' ', '\t', '\n', ',', ';',
-		'.', '!', '?', '\"', '(',
-		')', '{', '}', '\0'};
-
-	unsigned int i = 0;
-
-	while (array[i] != '\0')
-	{
-		if (array[i] == t)
-			return (array[i]);
-		++i;
-	}
-
-	return ('\0');
-}
-
-/**
  * cap_string - capitalizes first letter in a word
  * @s: string pointer to capitalize
  *
@@ -36,13 +11,23 @@ char *cap_string(char *s)
 
 	while (s[i] != '\0')
 	{
-		while (s[i] == check_condition(s[i]))
+		switch (s[i])
+		case ' ':
+		case '\t':
+		case '\n':
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '\"':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
 		{
-			if (s[i] == '\t')
-				s[i] == ' ';
-			++i;
-			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] -= 32;
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s[i + 1] -= 32;
 		}
 		++i;
 	}
