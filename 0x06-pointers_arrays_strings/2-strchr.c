eb4024a6fe5504cbd75f710bfa3f6a9a9a1c2a9b
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-05 21:22:45 +0000

    return null pointer and \0 if c is equal to \-

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,5 @@
 #include "holberton.h"
-/**
- * _strlen - returns the length of the string in the parameter
- * @s: the string
- *
- * Return: length of a string
- */
-
-int _strlen(char *s)
-{
-	int counter = 0;
-
-	while (s[counter] != '\0')
-	{
-		counter++;
-	}
-
-	return (counter);
-}
+#define NULL 0
 /**
  * _strchr - locates the first character in a string
  * and returns a pointer to it
@@ -28,16 +11,17 @@ int _strlen(char *s)
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int length, i;
-	length = _strlen(s);
-	i = 0;
+	unsigned int length;
 
-	while (i < length)
+	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
 		++s;
 	}
 
-	return (0);
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
 }
