6aed7d06efd74c59feb684747e697369916e324c
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-05 22:40:54 +0000

    use another function to check logic

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,22 @@
 #include "holberton.h"
 /**
+ * contains - returns 1 if char is found, 0 if not
+ * @x: character to find
+ * @acc: string to iterate though
+ *
+ * Return: 1 if found, 0 if not found
+ */
+int contains(char x, char *acc)
+{
+	while (*acc != '\0')
+	{
+		if (x == *acc)
+			return (1);
+		++acc;
+	}
+	return (0);
+}
+/**
  * _strpbrk - searches a string for a character, then
  * returns the pointer once the character is found
  * @s: the string to search
@@ -13,20 +30,12 @@ char *_strpbrk(char *s, char *accept)
 	unsigned int i, j;
 
 	i = 0;
-	while (*s != '\0')
+	while (*s)
 	{
-		j = 0;
-		while (accept[j] != '\0')
-		{
-			if (*s == accept[j])
-			{
-				return (s);
-			}
-			++j;
-		}
-		*(++s);
+		if (contains(*s, accept))
+			return (s);
+		++s;
 	}
-	*(s - 1);
 
 	return (0);
 }
