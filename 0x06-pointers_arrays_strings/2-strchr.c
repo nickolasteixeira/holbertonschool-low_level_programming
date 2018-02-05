cc5951605e1aa2a98827e21b8401036db3294632
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-05 18:31:45 +0000

    function that locates a character in a string

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strchr - locates the first character in a string
+ * and returns a pointer to it
+ * @s: string to look through
+ * @c: character to use to look through the s string
+ *
+ * Return: pointer if string is found
+ * Null if no character is found in the string
+ */
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		*(++s);
+	}
+
+	return (0);
+}
