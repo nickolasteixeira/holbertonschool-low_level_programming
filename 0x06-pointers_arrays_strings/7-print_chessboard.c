7ba70d365e8b0ba53951bd4c0a6d9095ca100922
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-05 23:25:29 +0000

    function that prints chessboard

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * print_chessboard - prints a chessboard of pieces
+ * @a: multidimensional loop
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j;
+
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			if (j != 8)
+			{
+				_putchar(a[i][j]);
+			}
+
+			if (j == 7)
+			{
+				_putchar(a[i][j]);
+				_putchar('\n');
+			}
+			++j;
+		}
+		++i;
+	}
+}
