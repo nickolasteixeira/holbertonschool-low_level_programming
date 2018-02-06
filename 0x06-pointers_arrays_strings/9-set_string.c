8c44d7998a37f971ae6b7c1c913c4e0dfc814412
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-06 01:53:21 +0000

    betty change

--- 0x06-pointers_arrays_strings/9-set_string.c
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
 /**
+ * set_string - sets the value of a pointer
+ * to a char
+ * @s: the double pointer
+ * @to: the single pointer
  *
- *
- *
- *
+ * Return: void;
  */
 void set_string(char **s, char *to)
 {
