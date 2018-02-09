269373effaf7cad9b86aa45badf6ae2df49f5d46
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-09 21:35:44 +0000

    program that multiplies all arguments and prints product after execution of script

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiples all numbers in argv and prints to output
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0;
+ */
+int main(int argc, char *argv[])
+{
+	unsigned int i;
+	int product;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (0);
+	}
+
+	i = 1;
+	product = 1;
+	while (i < argc)
+	{
+		product *= atoi(argv[i]);
+		++i;
+	}
+	printf("%d\n", product);
+	return (0);
+}
