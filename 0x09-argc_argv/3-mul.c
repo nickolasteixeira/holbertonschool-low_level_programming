39b6612a972c7b545333c4da76a755ca94ccfb2e
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-10 20:54:57 +0000

    add logic for non numeric values for argv[1] and argv[2]

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -2,6 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * check_arguments - checks if the string is a number or not
+ * @arg_v: string to check
+ *
+ * Return: 1 if it is, 0 if it isn't
+ */
+int check_arguments(char *arg_v)
+{
+	while (*arg_v != '\0')
+	{
+		if (!(*arg_v >= '0' && *arg_v <= '9'))
+		{
+			return (1);
+		}
+		*(++arg_v);
+	}
+	return (0);
+}
+
+/**
  * main - multiples all numbers in argv and prints to output
  * @argc: argument count
  * @argv: argument vector
@@ -19,13 +38,15 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	i = 1;
-	product = 1;
-	while (i < argc)
+	if (check_arguments(argv[1]) || check_arguments(argv[2]))
 	{
-		product *= atoi(argv[i]);
-		++i;
+		printf("Erorr\n");
+		return (1);
 	}
+
+	i = 0;
+	product = 0;
+	product = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", product);
 	return (0);
 }
