e4f2bddadfe2651d0c47d906e306c380527d992a
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-13 21:26:04 +0000

    update header file, add a function that returns a pointere to a newly allocated space in memory

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strlen - returns the length of a string, without '\0'
+ * @s: string to count
+ *
+ * Return: length of the string, int
+ */
+int _strlen(char *s)
+{
+	unsigned int counter = 0;
+
+	while (*s != '\0')
+	{
+		++counter;
+		++s;
+	}
+	return (counter);
+}
+/**
+ * _strdup - copies the string passed via the parameter
+ * @str: string to copy
+ *
+ * Return: a new pointer to the string that was copied
+ */
+char *_strdup(char *str)
+{
+	int length, i;
+	char *s;
+
+	length = _strlen(str);
+	s = malloc((length + 1) * sizeof(char));
+
+	if (*str == '\0')
+	{
+		s = NULL;
+		return (s);
+	}
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		s[i] = str[i];
+		++i;
+	}
+	return (s);
+}
