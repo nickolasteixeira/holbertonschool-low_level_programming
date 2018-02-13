9127ce1756fa3cbc5c6811a599506280798bfdc4
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-13 19:55:28 +0000

    change s address to null if size is 0

---
+++ 0x0A-malloc_free/0-create-array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array the size of int size
+ * with the character c
+ * @size: size of the array
+ * @c: character to add to the array
+ *
+ * Return: pointer to s
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s = (char*)malloc(size * sizeof(char));
+	unsigned int i = 0;
+	
+	if (size == 0)
+	{
+		s = NULL;
+		return (s);
+	}
+
+	while (i < size)
+	{
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return (s);
+}
