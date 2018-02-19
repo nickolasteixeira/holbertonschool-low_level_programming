70efbf94bfed51cf149bd4fb1ab708bbbb2b16b3
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-19 22:00:26 +0000

    store copies of name and owner to the heap

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -10,16 +10,22 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog_t *new_dog;
+	dog_t *new_dog;
+	dog_t *dog_copy;
 
 	new_dog = malloc(sizeof(struct dog_t));
-
 	if (new_dog == NULL)
 		return (NULL);
 
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
+
+	dog_copy = malloc(sizeof(struct dog_t));
+	if (dog_copy == NULL)
+		return (NULL);
+	dog_copy->name = name;
+	dog_copy->owner = owner;
 
 	return (new_dog);
 }
