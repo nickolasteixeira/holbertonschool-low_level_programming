f978013dd9fef53c211af07a75ed46822a44e4de
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-19 22:12:34 +0000

    function that frees memory from the heap

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees the memory used to create the pointer
+ * that points to the dog struct
+ * @d: the struct do free
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
