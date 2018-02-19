1a2a0e5fc2f8a87ff6c69bedc98df46ac5e67f5f
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-19 22:31:21 +0000

    malloc space for name and owner, redirect pointer to new memory addresses

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,23 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * _strlen - finds the length of a string
+ * @s: string to find the length
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s)
+	{
+		++i;
+		++s;
+	}
+	return (i);
+}
+/**
  * new_dog - initializes a struct dog_t new_dog
  * @name: dog's name
  * @age: dog's age
@@ -11,14 +28,23 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *n;
+	char *o;
 
 	new_dog = malloc(sizeof(struct dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	new_dog->name = name;
+	n = malloc(_strlen(name) * sizeof(char));
+	o = malloc(_strlen(owner) * sizeof(char));
+	if (n == NULL)
+		return (NULL);
+	if (o == NULL)
+		return (NULL);
+
+	new_dog->name = n;
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = o;
 
 	return (new_dog);
 }
