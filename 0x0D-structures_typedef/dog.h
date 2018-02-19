810a5cab86f7a5d9ee4e5c277370973675517259
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-19 20:27:03 +0000

    header file with the struct dog information

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - description of a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
