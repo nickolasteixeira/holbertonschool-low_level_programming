1c4541358221ed1d0f5144d480cdf0cb1ca03206
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-19 21:47:02 +0000

    function that creates a new typedef dog_t and initializes all it's values

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - initializes a struct dog_t new_dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner's name
+ *
+ * Return: pointer to the dog_t struct that was created
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog_t *new_dog;
+
+	new_dog = malloc(sizeof(struct dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+	return (new_dog);
+}
