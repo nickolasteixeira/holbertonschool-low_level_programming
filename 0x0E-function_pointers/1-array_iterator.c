f94f849f3fdca4d8a8e8d89537eba1a08bc4dcc1
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-21 20:55:53 +0000

    remove edge cases

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,6 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
-#include <stdio.h>
 /**
  * array_iterator - iterates through the array that is passed
  * and uses a callback function
@@ -14,12 +12,9 @@ void array_iterator(int *array, size_t size, void(*action)(int))
 {
 	unsigned int i = 0;
 
-	if (array == NULL || size == NULL || action == NULL)
-		return;
-
 	while (i < size)
 	{
-		action(array[i]);
+		(*action)(array[i]);
 		++i;
 	}
 }
