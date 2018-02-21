e7ea5964c6b9ea2d4b971a4796dd1592df5d71eb
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-21 18:03:43 +0000

    initialize readme, add headerfile, add function that prints a name using a callback function

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints the name of a person
+ * using the void f callback function
+ * @name: name of person
+ * @f: call back function that prints name
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
