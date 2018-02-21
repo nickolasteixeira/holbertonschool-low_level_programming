b7be70972f20e00e7632e4ff3538302c83992392
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-21 23:32:51 +0000

    moved logic to if condition

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -22,9 +22,9 @@ int (*get_op_func(char *s))(int a, int b)
 	int i;
 
 	i = 0;
-	while (ops[i].op != NULL && s[1] == '\0')
+	while (ops[i].op != NULL)
 	{
-		if (*s == ops[i].op[0])
+		if (*s == ops[i].op[0] && s[1] == '\0')
 			return (ops[i].f);
 		++i;
 	}
