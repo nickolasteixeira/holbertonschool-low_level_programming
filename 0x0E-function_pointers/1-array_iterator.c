479e12dbd49304dbdbd29fb698f4a20952e34c6f
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-21 18:27:16 +0000

    update header file, function that executes a funciton given as a parameter on each element of an array

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * array_iterator - iterates through the array that is passed
+ * and uses a callback function
+ * @array: array to iterate
+ * @size: size of array
+ * @action: callback function being passed
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+	unsigned int i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		++i;
+	}
+}
