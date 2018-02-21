af28c380f1fef245001b8544653dd2b58cca31ac
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-21 22:25:06 +0000

    typo comp to cmp

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	int i, check;
 
-	if (array == NULL || comp == NULL)
+	if (array == NULL || cmp == NULL)
 		return (-1);
 
 	/* edge case */
