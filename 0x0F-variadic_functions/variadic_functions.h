f397ade7c02d13b07fb8302cf245a6577b167f38
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-02-24 01:44:11 +0000

    header file update with structs and function helpers

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -7,4 +7,15 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+typedef struct dtypes 
+{
+	char *c;
+	void (*f)();
+} all_list;
+
+void print_c();
+void print_i();
+void print_f();
+void print_s();
 #endif /* _VARIADIC_FUNCTIONS_H_ */
