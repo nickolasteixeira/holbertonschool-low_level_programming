1e290929b8fc7c295ac94c0ee3427bd0fb47f0dc
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-03-06 18:25:56 +0000

    remove *head to check just for head, NULL

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *copy, *after;
 	unsigned int counter;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
