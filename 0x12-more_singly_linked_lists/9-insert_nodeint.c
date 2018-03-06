af09d023b68209b66b71d60a4825adc7910ca1e4
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-03-06 19:47:51 +0000

    change idx to 0 in if statement

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	counter = 0;
 	copy = *head;
 
-	if (counter == idx)
+	if (counter == 0)
 	{
 		new->n = n;
 		new->next = copy->next;
