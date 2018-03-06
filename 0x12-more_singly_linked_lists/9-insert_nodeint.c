ab2f6e5c9eaf8edb18bfd8c8829dda30ef77ebb1
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-03-06 19:03:36 +0000

    free if new struct is NULL

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			copy = copy->next;
 			if (copy == NULL)
+			{
+				free(new);
 				return (NULL);
+			}
 			++counter;
 		}
 
