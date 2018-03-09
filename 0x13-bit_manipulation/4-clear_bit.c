a6fcb2ecb1bc9941aedbfded04f9151db8f6e298
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-03-09 01:17:22 +0000

    check for null case before maxsize

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -15,17 +15,14 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	mask = 1;
 	maxsize = sizeof(n) * 8;
 
-	if (index > maxsize)
+	if (*n == '\0')
 		return (-1);
 
-	if (*n == '\0')
+	if (index > maxsize)
 		return (-1);
 
 	*n = ~*n;
 	*n = *n | mask << index;
 	*n = ~*n;
-	if (*n)
-		return (1);
-	else
-		return (-1);
+	return (1);
 }
