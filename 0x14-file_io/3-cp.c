9ffe9dd6b0872cc8e9a7abbeb7cdba1344ab1b2e
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-03-13 23:50:26 +0000

    check for failing cases for open, close, read, write

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -4,18 +4,23 @@
 
 int read_a_textfile(const char *file_from, char *file_to)
 {
-	int fd_to, fd_from, actual_read, actual_write, actual_close_to, actual_close_from;
+	int fd_to, fd_from, actual_read, actual_write;
 	char *buff;
 
 	/* opening files from the arguments we passed to the function */
 	fd_from = open(file_from, O_RDWR);
 	if (fd_from == -1)
-		return (-1);
-
-	fd_to = open(file_to, O_CREAT | O_RDWR | O_TRUNC, (S_IRUSR | S_IWUSR | S_IRGRP
-				| S_IWGRP | S_IROTH)); 
+	{
+		dprintf(STDERR_FILENO, "Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 
+			(S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)); 
 	if (fd_to == -1)
-		return (-1);
+	{
+		dprintf(STDERR_FILENO, "Can't write to %s\n", file_to);
+		exit(99);		
+	}
 
 	/* creating a buffer to store information to write to the new file */
 	buff = malloc(sizeof(char) * 1024);
@@ -27,25 +32,25 @@ int read_a_textfile(const char *file_from, char *file_to)
 	if (actual_read == -1)
 	{
 		free(buff);
-		return (0);
+		dprintf(STDERR_FILENO, "Can't read from file %s\n", file_from);
+		exit(98);	
 	}
 	/* writing to the filename you wanted to create at 1024 bytes at a time */
 	actual_write = write(fd_to, buff, 1024);
 	if (actual_write == -1)
 	{
 		free(buff);
-		return (-1);
+		dprintf(STDERR_FILENO, "Can't write %s\n", file_to);
+		exit(99);
 	}
 	
 	/*closing the file and checking for edgecases */
-	actual_close_to = close(fd_to);
-	actual_close_from = close(fd_from);
-	if (actual_close_to == -1)
+	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-	if (actual_close_from == -1)
+	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
@@ -61,7 +66,7 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[1], argv[2]);
+		dprintf(STDERR_FILENO, "Usage: cp %s %s", argv[1], argv[2]);
 		exit(97);
 	}
 
