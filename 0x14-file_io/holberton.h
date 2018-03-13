ce45043dada53135270f3fded9695149a927b615
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-03-13 19:25:07 +0000

    typo in header file

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -4,7 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unist.h>
+#include <unistd.h>
 #include <stdlib.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
