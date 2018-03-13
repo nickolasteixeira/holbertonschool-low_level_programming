fa55aa4a31b60e3616972c00e8553ce8cf94766b
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-03-13 19:22:49 +0000

    include header files for system calls

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -1,6 +1,11 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unist.h>
+#include <stdlib.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
