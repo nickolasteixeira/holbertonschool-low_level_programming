fa5a0322d1cfe86a53e6a89c52aed53063ef7c9c
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-03-13 21:10:04 +0000

    add flag for O_TRUNC, add condition for text_content = NULL

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -27,11 +27,15 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, size, actual_write;
 
-	size = _strlen(text_content);
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY, (S_IRUSR | S_IWUSR));
+	if (text_content == NULL)
+		text_content = "";
+
+	size = _strlen(text_content);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, (S_IRUSR | S_IWUSR));
 	if (fd == -1)
 		return (-1);
 
