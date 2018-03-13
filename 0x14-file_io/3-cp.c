1a6f7a9ba4de532b7c33aa7eb1565517150d2e32
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-03-13 23:31:25 +0000

    program that capies the content of a file to another file

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,73 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int read_a_textfile(const char *file_from, char *file_to)
+{
+	int fd_to, fd_from, actual_read, actual_write, actual_close_to, actual_close_from;
+	char *buff;
+
+	/* opening files from the arguments we passed to the function */
+	fd_from = open(file_from, O_RDWR);
+	if (fd_from == -1)
+		return (-1);
+
+	fd_to = open(file_to, O_CREAT | O_RDWR | O_TRUNC, (S_IRUSR | S_IWUSR | S_IRGRP
+				| S_IWGRP | S_IROTH)); 
+	if (fd_to == -1)
+		return (-1);
+
+	/* creating a buffer to store information to write to the new file */
+	buff = malloc(sizeof(char) * 1024);
+	if (buff == NULL)
+		return (0);
+
+	/* reading file 1024 bytes at a time */
+	actual_read = read(fd_from, buff, 1024);
+	if (actual_read == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	/* writing to the filename you wanted to create at 1024 bytes at a time */
+	actual_write = write(fd_to, buff, 1024);
+	if (actual_write == -1)
+	{
+		free(buff);
+		return (-1);
+	}
+	
+	/*closing the file and checking for edgecases */
+	actual_close_to = close(fd_to);
+	actual_close_from = close(fd_from);
+	if (actual_close_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+	if (actual_close_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	/* freeing the buffer from the heap */
+	free(buff);
+	return (1);
+
+}	
+int main(int argc, char **argv)
+{
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[1], argv[2]);
+		exit(97);
+	}
+
+	read_a_textfile(argv[1], argv[2]);
+	/* some condition to check if returned properply */
+	
+
+	return (0);
+}
