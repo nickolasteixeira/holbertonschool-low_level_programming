6dca471997f3f806ff7249f8ef9121c34ea9bcef
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-03-14 00:02:26 +0000

    typo with error messages

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -4,35 +4,35 @@
 
 int read_a_textfile(const char *file_from, char *file_to)
 {
-	int fd_to, fd_from, actual_read, actual_write;
+	int fd_to, fd_from, actual_read, actual_write, ret;
 	char *buff;
 
 	/* opening files from the arguments we passed to the function */
 	fd_from = open(file_from, O_RDWR);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 
 			(S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)); 
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);		
 	}
 
 	/* creating a buffer to store information to write to the new file */
 	buff = malloc(sizeof(char) * 1024);
 	if (buff == NULL)
-		return (0);
+		return (-1);
 
 	/* reading file 1024 bytes at a time */
 	actual_read = read(fd_from, buff, 1024);
 	if (actual_read == -1)
 	{
 		free(buff);
-		dprintf(STDERR_FILENO, "Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);	
 	}
 	/* writing to the filename you wanted to create at 1024 bytes at a time */
@@ -40,17 +40,19 @@ int read_a_textfile(const char *file_from, char *file_to)
 	if (actual_write == -1)
 	{
 		free(buff);
-		dprintf(STDERR_FILENO, "Can't write %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
 	
 	/*closing the file and checking for edgecases */
-	if (close(fd_to) == -1)
+	ret = close(fd_to);
+	if (ret == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-	if (close(fd_from) == -1)
+	ret = close(fd_from);
+	if (ret == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
