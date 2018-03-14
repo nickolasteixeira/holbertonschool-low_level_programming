ae621abb7be248b589d282af7f172681c2377162
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-03-14 01:15:29 +0000

    update header file

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -6,6 +6,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+void print_error_98(char *file_from);
+void print_error_99(char *file_to);
+void print_error_100(int fd);
+void read_a_textfile(char *file_from, char *file_to);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
