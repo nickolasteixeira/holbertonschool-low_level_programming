ae03f4a47746a2e18b53518baa8db9931957d0be
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-03-14 01:13:37 +0000

    create new functions for error statuses, betty update

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,6 +1,35 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
+/**
+ * print_error_98 - prints an error message
+ * @file_from: the file name to print
+ * Return: void;
+ */
+void print_error_98(char *file_from)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+	exit(98);
+}
+/**
+ * print_error_99 - prints an error message
+ * @file_to: the file name to print
+ * Return: void;
+ */
+void print_error_99(char *file_to)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+	exit(99);
+}
+
+/**
+ * print_error_100 - prints an error message
+ * @fd: the file descriptor to print
+ * Return: void;
+ */
+void print_error_100(int fd)
+{
+	dprintf(STDERR_FILENO, "Can't close file fd %d\n", fd);
+	exit(100);
+}
 /**
  * read_a_textfile - reads a file, then makes a copy to another file
  * @file_from: the file to read from
@@ -8,54 +37,38 @@
  *
  * Return: 1 if successful
  */
-int read_a_textfile(char *file_from, char *file_to)
+void read_a_textfile(char *file_from, char *file_to)
 {
 	int fd_to, fd_from, actual_read;
 	char buff[1024];
 
-	/* opening files from the arguments we passed to the function */
-	fd_from = open(file_from, O_RDWR);
+	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+		print_error_98(file_from);
+
 	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC,
 			(S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH));
 	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
+		print_error_99(file_to);
+
 	/* reading file 1024 bytes at a time */
 	while ((actual_read = read(fd_from, buff, 1024)) > 0)
 	{
 		if (actual_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-			exit(98);
-		}
+			print_error_98(file_from);
 		/* writing to the filename you wanted to create at 1024 bytes at a time */
 		actual_read = write(fd_to, buff, actual_read);
 		if (actual_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
+			print_error_99(file_to);
 	}
-
+	/*checking if the condition statement for the while loop errors */
+	if (actual_read == -1)
+		print_error_98(file_from);
 	/*closing the file and checking for edgecases */
 	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+		print_error_100(fd_to);
 	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	return (1);
+		print_error_100(fd_from);
 }
 /**
  * main - calls the read_a_textfile function
