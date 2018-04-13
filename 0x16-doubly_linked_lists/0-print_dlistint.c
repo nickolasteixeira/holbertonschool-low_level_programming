618c775ca798d2f17988591231627bd161df4a77
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-04-13 17:37:40 +0000

    prints all elements of a doubly linked list

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints a doubly linked list's value, returns
+ * number of nodes
+ * @h: head of the doubly linked list
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t num_nodes;
+    const dlistint_t *copy;
+
+    num_nodes = 0, copy = h;
+
+    if (h == NULL)
+        return (num_nodes);
+
+    while (copy)
+    {
+        num_nodes += 1;
+        printf("%d\n", copy->n);
+        copy = copy->next;   
+    }
+    return (num_nodes);
+}
