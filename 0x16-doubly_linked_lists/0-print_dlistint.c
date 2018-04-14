a239dc41cc963255c285506a4497a6ac1d2859d6
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-04-14 20:51:55 +0000

    new file, betty compliant

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -2,25 +2,25 @@
 /**
  * print_dlistint - prints a doubly linked list's value, returns
  * number of nodes
- * @h: head of the doubly linked list
+ * @h: head of doubly linked list
  *
  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t num_nodes;
-    const dlistint_t *copy;
+	size_t num_nodes;
+	const dlistint_t *copy;
 
-    num_nodes = 0, copy = h;
+	num_nodes = 0, copy = h;
 
-    if (h == NULL)
-        return (num_nodes);
+	if (h == NULL)
+		return (num_nodes);
 
-    while (copy)
-    {
-        num_nodes += 1;
-        printf("%d\n", copy->n);
-        copy = copy->next;   
-    }
-    return (num_nodes);
+	while (copy)
+	{
+		num_nodes += 1;
+		printf("%d\n", copy->n);
+		copy = copy->next;
+	}
+	return (num_nodes);
 }
