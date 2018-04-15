b0d508dc3da7134d0a5b00e5a1767aef33e86c80
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-04-15 23:28:17 +0000

    prev direction change for position 0

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new->prev = NULL;
 		new->next = *h;
-		(*h)->next->prev = new;
+		(*h)->prev = new;
 		*h = new;
 		return (new);
 	}
