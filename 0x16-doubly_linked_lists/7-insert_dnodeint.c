4d52e85968681b8448b0bcf165b16e6df43b2d8e
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-04-15 23:19:30 +0000

    logic change for inserting node at position 0

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new->prev = NULL;
-		new->next = (*h)->next;
+		new->next = *h;
 		(*h)->next->prev = new;
 		*h = new;
 		return (new);
