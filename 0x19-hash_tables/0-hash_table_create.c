23b2ee1759f6933a348432761714716293e16990
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-05-10 19:49:06 +0000

    change malloc size to char * instead of the hash_node_t size

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = malloc(sizeof(hash_table_t) * size);
+	table->array = malloc(sizeof(char *) * size);
 	if (table->array == NULL)
 	{
 		free(table);
