535f3e63d99c1ce9ad6194e084e24823e9967b3d
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-05-11 18:20:26 +0000

    refactor code to check if key exists first

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -17,15 +17,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	const unsigned char *new_key = (const unsigned char *) key;
 
 	index = key_index(new_key, ht->size);
-	if (ht->array[index] == NULL)
+	head = ht->array[index];
+	if (head)
 	{
+		while (head)
+		{
+			if (strcmp(head->key, key) == 0)
+			{
+				head->value = strdup(value);
+				return (1);
+			}
+			head = head->next;
+		}
+
 		new_node = malloc(sizeof(hash_node_t));
 		if (new_node == NULL)
 			return (0);
+		printf("Index: %lu\n", index);
 		new_node->key = strdup(key);
 		if (new_node->key == NULL)
 		{
-			free(new_node)
+			free(new_node);
 			return (0);
 		}
 		new_node->value = strdup(value);
@@ -34,30 +46,28 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(new_node);
 			return (0);
 		}
-		new_node->next = NULL;
+		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
-		printf("New Index: %lu\n", index);
 		return (1);
 	}
 
-	head = ht->array[index];
-	while (head)
-	{
-		if (strcmp(head->key, key) == 0)
-		{
-			head->value = strdup(value);
-			return (1);
-		}
-		head = head->next;
-	}
-	
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	printf("Index: %lu\n", index);
 	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
 	new_node->value = strdup(value);
-	new_node->next = ht->array[index];
+	if (new_node->value == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->next = NULL;
 	ht->array[index] = new_node;
+	printf("New Index: %lu\n", index);
 	return (1);
 }
