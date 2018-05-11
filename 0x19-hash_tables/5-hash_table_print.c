f6ef2e5136a8284f5134c77d1f980ba68e306567
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-05-11 18:57:32 +0000

    print hashtable, still an error a ',' error at last key value

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *runner;
+	unsigned long int index;
+	
+	index = 0;
+	printf("{");
+	while(index < ht->size)
+	{
+		runner = ht->array[index];
+		if (runner)
+		{
+			while(runner)
+			{
+				printf("'%s': '%s', ", runner->key, runner->value);
+				runner = runner->next;
+			}
+		}
+		++index;
+	}
+	printf("}\n");
+}
