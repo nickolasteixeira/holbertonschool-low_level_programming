fefa1787d92580fbdff35165428aaa5a47ad16ec
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-05-11 21:59:44 +0000

    prints hash table without , at the last key, value pair

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -1,24 +1,36 @@
 #include "hash_tables.h"
 #include <stdio.h>
 /**
+ * hash_table_print - prints the entire hashed tabled
+ * @ht: the pointer to the first element in the hash table
  *
- *
- *
- *
+ * Return: void
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *runner;
-	unsigned long int index;
-	
+	hash_node_t *runner, *last_node;
+	unsigned long int index, last_index;
+
+
+	last_index = (ht->size) - 1;
+	while (last_index != 0)
+	{
+		last_node = ht->array[last_index];
+		if (last_node)
+			break;
+		--last_index;
+	}
+
 	index = 0;
 	printf("{");
-	while(index < ht->size)
+	while (index < ht->size)
 	{
 		runner = ht->array[index];
-		while(runner)
+		while (runner)
 		{
-			printf("'%s': '%s', ", runner->key, runner->value);
+			printf("'%s': '%s'", runner->key, runner->value);
+			if (runner != last_node)
+				printf(",");
 			runner = runner->next;
 		}
 		++index;
