7333c195e12eddf61066a8399a5b71db2888c44a
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-05-29 17:27:54 +0000

    quicksort and time/space complexity

--- 0x1A_sorting_algorithms/3-quick_sort.c
+++ 0x1A_sorting_algorithms/3-quick_sort.c
@@ -1,4 +1,11 @@
 #include "sort.h"
+/**
+ * swap - swaps the values of two different memory locations
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: void
+ */
 void swap(int *a, int *b)
 {
 	int temp;
@@ -9,12 +16,15 @@ void swap(int *a, int *b)
 }
 
 /**
- *
- *
+ * partition - partitions the array
+ * @array: the array to partition
+ * @start: the starting position
+ * @end: the ending position
+ * @n: the length of the array
  *
  * Return: the index of the pivot
  */
- int partition(int *array, size_t start, size_t end, size_t n)
+int partition(int *array, size_t start, size_t end, size_t n)
 {
 	int pivot, p_idx;
 	unsigned int i;
@@ -22,58 +32,61 @@ void swap(int *a, int *b)
 	p_idx = start;
 	pivot = array[end];
 
-	printf("Start %d Pivot %d End %lu\n", p_idx, pivot, end);	
 	for (i = start; i < end; i++)
 	{
-		if (array[i] <= pivot)
+		if (array[i] < pivot)
 		{
-			printf("array[i] %d\n", array[i]);
-			swap(&array[i], &array[p_idx]);
-			print_array(array, n);
-			p_idx += 1;
-		}	
+			if (i != start)
+			{
+				swap(&array[p_idx], &array[i]);
+				print_array(array, n);
+				p_idx += 1;
+			}
+		}
 	}
-	printf("P_IDX %d\n", p_idx);
 	if (array[p_idx] > array[end])
 	{
 		swap(&array[p_idx], &array[end]);
 		print_array(array, n);
 	}
-	printf("-------END Partition-------\n\n\n");
-	return p_idx;	
+	return (p_idx);
 }
 
 /**
+ * q_sort - calls itself recursively to partition the array
+ * @array: array to sort
+ * @start: start of the array
+ * @end: end of the array
+ * @size: length of the array
  *
- *
- *
- *
+ * Return: void
  */
 void q_sort(int *array, size_t start, size_t end, size_t size)
 {
 	size_t p_idx;
 
+	if (end == 0 || end <= start)
+		return;
+
 	if (start < end)
 	{
 		p_idx = partition(array, start, end, size);
-		printf("Partition: %lu\n", p_idx);
 		if (p_idx > 0 && p_idx > start)
-		{
-			printf("Enterered\n");
 			q_sort(array, start, p_idx - 1, size);
-		}
 		if (p_idx < size - 1)
 			q_sort(array, p_idx + 1, end, size);
 	}
 }
 /**
-  *
-  *
-  *
-  *
-  */
+ * quick_sort - calls the q_sort method to partition
+ * @array: array to sort
+ * @size: length of the array
+ *
+ * Return: void
+ */
 void quick_sort(int *array, size_t size)
 {
+	if (array ==  NULL || size == 0 || size == 1)
+		return;
 	q_sort(array, 0, size - 1, size);
-}	
-
+}
