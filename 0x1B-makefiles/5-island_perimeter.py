6b4b2309a4ef59ea0f3ab435272ebcb8f653a82f
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-06-19 01:33:46 +0000

    modify algo structure for perimeter problem

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -2,44 +2,15 @@
 
 
 def island_perimeter(grid):
+    """finds the perimeter of number of 0's around 1's in a grid"""
     count = 0
-    for i, v in enumerate(grid):
-        for j, v in enumerate(v):
-            # corner edge cases
-            if i == 0 and j == 0 and v == 1:
-                count += 2
-            if i == 0 and j == len(grid[i])-1 and v == 1:
-                count += 2
-            if i == len(grid)-1 and j == 0 and v == 1:
-                count += 2
-            if i == len(grid)-1 and j == len(grid[i])-1 and v == 1:
-                count += 2
-            # row and columns at the end of the grid
-            if i == 0 and (j != 0 or j != len(grid[i])-1) and v == 1:
-                if grid[i][j-1] == 0:
-                    count += 1
-                if grid[i][j+1] == 0:
-                    count += 1
-            if i == len(grid)-1 and (j != 0 or j != len(grid[i])-1) and v == 1:
-                if grid[i][j-1] == 0:
-                    count += 1
-                if grid[i][j+1] == 0:
-                    count += 1
-            if j == 0 and v == 1:
-                if grid[i][j+1] == 0:
-                    count += 1
-            if j == len(grid[i])-1 and v == 1:
-                if grid[i][j-1] == 0:
-                    count += 1
 
-            # any columns or rows that are not the outer perimter
-            if (i != 0 or i != len(grid)-1) and (j != 0 or j != len(grid[i])-1):
-                if j == 1:
-                    if grid[i][j-1] == 0:
-                        count += 1
-                    if grid[i][j+1] == 0:
-                        count += 1
-                    if grid[i-1][j] == 0:
-                        count += 1
-
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                count += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    count -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    count -= 2
     return count
