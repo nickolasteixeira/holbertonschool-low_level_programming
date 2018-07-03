d5f9ee7c9114ddf1f25dd59d4ada0329bf18dfe2
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-07-03 17:40:41 +0000

    change NULL condition to return 0, not 1

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -7,17 +7,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-
 	size_t lheight, rheight;
 
-	if (tree == NULL)
-		return (-1);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
 
-	lheight = binary_tree_height(tree->left) + 1;
-	rheight = binary_tree_height(tree->right) + 1;
+	lheight = binary_tree_height(tree->left);
+	rheight = binary_tree_height(tree->right);
 
 	if (lheight > rheight)
-		return (lheight);
+		return (lheight + 1);
 	else
-		return (rheight);
+		return (rheight + 1);
 }
