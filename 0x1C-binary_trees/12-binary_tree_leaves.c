027c68e5651ced88aa5d82a94198c829e6f5f4d0
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-07-05 17:35:44 +0000

    find all leaves in a btree

--- 0x1C-binary_trees/12-binary_tree_leaves.c
+++ 0x1C-binary_trees/12-binary_tree_leaves.c
@@ -7,13 +7,11 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t counter;
+	if (tree == NULL)
+		return (0);
 
-	counter = 0;
-	if (tree->left)
-		counter++;
-	if (tree->right)
-		counter++;
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-	return (counter);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
