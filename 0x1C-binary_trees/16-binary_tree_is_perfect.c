cc29e9d27b8c82ab496dc71e7af33f3bd759c7e3
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-07-06 23:45:08 +0000

    checking for a perfect tree

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -1,19 +1,51 @@
 #include "binary_trees.h"
 #include "15-binary_tree_is_full.c"
 #include "14-binary_tree_balance.c"
+
+/**
+ * check_level - checks if leaves are at same level
+ * @tree: tree to check
+ * @current: current level
+ * @original: original level
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+
+int check_level(const binary_tree_t *tree, int current, int original)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->right == NULL && tree->left == NULL)
+	{
+		if (original == 0)
+		{
+			original = current;
+			return (1);
+		}
+		return (original == current);
+	}
+
+	return (check_level(tree->left, current + 1, original) &&
+		check_level(tree->right, current + 1, original));
+}
+
 /**
  * binary_tree_is_perfect - checks if tree is perfect
  *
  * @tree: node to start the check
  * Return: 0 if not, 1 if perfect
  */
+
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int level = 0;
+
 	if (tree == NULL)
 		return (0);
 
 	if (binary_tree_is_full(tree) && binary_tree_balance(tree) == 0)
-		return (1);
+		return (check_level(tree, 0, level));
 
 	return (0);
 }
--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -30,7 +30,7 @@ void binary_tree_print(const binary_tree_t *);
 /* helper functions */
 int find_balance(const binary_tree_t *tree);
 
-/* 0 - 18 */
+/* mandatory */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -52,8 +52,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 /* advanced */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
-
 
 #endif /* _Binary_Trees_ */
