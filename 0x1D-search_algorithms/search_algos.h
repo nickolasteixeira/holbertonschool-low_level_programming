45108ea7449aab1b1c2093de1547876a28fab73f
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-08-08 15:50:55 +0000

    betty header

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -9,7 +9,7 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 
 /* Helper functions */
-int find_value(int *array, unsigned int i, unsigned int end, int value, size_t size);
+int find_value(int *array, unsigned int i, unsigned int e, int v, size_t s);
 int binary_S(int *array, size_t l, size_t r, int value);
 void print_array(int *array, size_t l, size_t r);
-#endif /* _Search Algos */ 
+#endif /* _Search Algos */
