c17db0230450f87d655b993054c06349cfd94eb1
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-08-08 17:02:19 +0000

    type

--- 0x1D-search_algorithms/102-interpolation.c
+++ 0x1D-search_algorithms/102-interpolation.c
@@ -31,6 +31,6 @@ int interpolation_search(int *array, size_t size, int value)
 
 	pos = low + (((double)(high - low) /
 		      (array[high] - array[low])) * (value - array[low]));
-	printf("Value check at array[%lu] is out of range\n", pos);
+	printf("Value checked array[%lu] is out of range\n", pos);
 	return (-1);
 }
