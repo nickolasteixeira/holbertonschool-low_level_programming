fad3f4fcfa0d09c0fcd43d9d0f6982084a24829f
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-08-08 21:57:07 +0000

    function that searches for a value in a sorted list of integers using the Jump search algorithm

--- 0x1D-search_algorithms/105-jump_list.c
+++ 0x1D-search_algorithms/105-jump_list.c
@@ -2,98 +2,89 @@
 #include <stdio.h>
 #include <math.h>
 /**
+ * jump_check - checks jump value in singly list
+ * @jump_temp: pointer to use to jump
+ * @jump: size of the jump value
  *
- *
- *
- *
+ * Return: -1 if hitting NULL, 1 if able to jump
  */
 int jump_check(listint_t *jump_temp, size_t jump)
 {
 	size_t i;
-		
+
 	i = 0;
 	while (i < jump)
 	{
-		if (jump_temp == NULL)
+		if (jump_temp->next == NULL)
 			return (-1);
 		jump_temp = jump_temp->next;
+		++i;
 	}
 	return (1);
 }
-/*
-void check_indexes(listint_t *j_temp, size_t jump, int value)
-{
-	size_t i;
 
-	i = 0;
-	while (i < jump)
-	{
-		printf("Value checked at index[%lu] = [%u]\n", j_temp->index, j_temp->n);
-		if (value == j_temp->n)
-			break;
-		++i;
-	}
-}
-*/
-void check_values(listint_t *j_temp, size_t jump, int value)
-{
-	size_t i;
-	listint_t *temp;
-
-	temp = j_temp;
-	
-	printf("Value checked at index [%lu] = [%u]\n", j_temp->index, j_temp->n);
-	i = 0;
-	while (i < jump)
-	{
-		if (value == j_temp->n)
-		{
-			printf("Value found between indexes [%lu] and [%lu]\n", temp->index, temp->index + jump);
-			/*check_indexes(j_temp, jump, value);*/
-			break;
-		}
-		else
-		{
-			temp = temp->next;
-			++i;
-		}
-	}
-}
 /**
+ * jump_list - function that searches for a value in a sorted list of integers
+ * @list: list to search
+ * @size: size to jump
+ * @value: value to search for
  *
- *
- *
- *
+ * Return: NUll if not found, index pointer if found
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t jump, pass, i, j;
-	listint_t *temp, *jump_temp;
+	size_t jump, i, j;
+	int found, pass;
+	listint_t *temp, *j_temp;
 
 	if (list == NULL)
 		return (NULL);
 
-	temp = list, jump = sqrt(size), i = 0;
+	temp = list, jump = sqrt(size), i = 0, found = 0;
 	while (i < size)
 	{
-		jump_temp = temp;	
-		pass = jump_check(jump_temp, jump);
-		if (pass)
+		j_temp = temp;
+		pass = jump_check(j_temp, jump);
+		if (pass == 1)
 		{
 			j = 0;
+			j_temp = temp;
 			while (j < jump)
 			{
+				if (value == temp->n)
+					found = 1;
 				temp = temp->next;
 				++j;
 			}
-			/*jump_temp = temp;*/
-			check_values(temp, jump, value);		
+			printf("Value checked at index [%lu] = [%u]\n", temp->index, temp->n);
+			if (found)
+			{
+				printf("Value found between indexes[%lu] = [%lu]\n", j_temp->index, temp->index);
+				j = 0;
+				while (j < jump)
+				{
+					printf("Value check at index [%lu] = [%u]\n", j_temp->index, j_temp->n);
+					if (value == j_temp->n)
+						return (j_temp);
+					j_temp = j_temp->next;
+				}
+				return (NULL);
+			}
+			i += jump;
 		}
 		else
-			break;
-
-		i += jump;
+		{
+			while (temp)
+			{
+				printf("Value check at index [%lu] = [%u]\n", temp->index, temp->n);
+				if (temp == NULL)
+					return (NULL);
+				if (value == temp->n)
+					return (temp);
+				temp = temp->next;
+			}
+			return (NULL);
+		}
 	}
-		
-	return (list);
+	return (NULL);
 }
