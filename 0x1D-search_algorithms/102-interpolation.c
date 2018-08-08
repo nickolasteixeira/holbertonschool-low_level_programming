0436f1ee16aaf64b5b48449abcda1f8671ccabe9
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-08-08 17:00:37 +0000

    print new pos if not found before print statement

--- 0x1D-search_algorithms/102-interpolation.c
+++ 0x1D-search_algorithms/102-interpolation.c
@@ -28,5 +28,9 @@ int interpolation_search(int *array, size_t size, int value)
 		else
 			high = pos - 1;
 	}
+
+	pos = low + (((double)(high - low) /
+		      (array[high] - array[low])) * (value - array[low]));
+	printf("Value check at array[%lu] is out of range\n", pos);
 	return (-1);
 }
